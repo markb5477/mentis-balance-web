24d9e1cc28c69a78f7e2161c28935f6541c38817
Mark <mark.nicklas.brandt@gmail.com>
2025-10-08 14:20:57 +0200

    Increase map size and adjust zoom level on contact page
    - Increased map height from 300px to 500px for better visibility
    - Adjusted zoom level to show more surrounding area

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -111,9 +111,9 @@ ${formData.message}`;
                 <Card>
                   <CardContent className="p-0">
                     <iframe
-                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2249.1234567890123!2d12.5563!3d55.6981!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTXCsDQxJzUzLjIiTiAxMsKwMzMnMjIuNyJF!5e0!3m2!1sda!2sdk!4v1234567890123!5m2!1sda!2sdk"
+                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4498.246913578024!2d12.5563!3d55.6981!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f21!3m3!1m2!1s0x0%3A0x0!2zNTXCsDQxJzUzLjIiTiAxMsKwMzMnMjIuNyJF!5e0!3m2!1sda!2sdk!4v1234567890123!5m2!1sda!2sdk"
                       width="100%"
-                      height="300"
+                      height="500"
                       style={{ border: 0 }}
                       allowFullScreen
                       loading="lazy"
