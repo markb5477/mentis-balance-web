ab1903521b8eb73d6ef1c6d56e2c92f9e83a7150
Mark <mark.nicklas.brandt@gmail.com>
2025-10-08 13:19:44 +0200

    Fix GitHub Pages deployment: use HashRouter and update workflow

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./i18n";
 import Index from "./pages/Index";
 import Profile from "./pages/Profile";
@@ -21,7 +21,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/profil" element={<Profile />} />
@@ -49,7 +49,7 @@ const App = () => (
           <Route path="/vilkar" element={<Terms />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
